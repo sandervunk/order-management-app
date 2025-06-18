fd63a0bbcefe34f657f799654f9a2630fdb3744e
Sander Vunk <sandervunk24@hotmail.com>
2025-06-18 17:58:12 +0300

    fix: fixed failing tests

--- backend/src/orders/orders.controller.spec.ts
+++ backend/src/orders/orders.controller.spec.ts
@@ -1,14 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './entities/order.entity';
 
 describe('OrdersController', () => {
   let controller: OrdersController;
 
+  const mockOrderRepository: Partial<Repository<Order>> = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(Order),
+          useValue: mockOrderRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<OrdersController>(OrdersController);
--- backend/src/orders/orders.service.spec.ts
+++ backend/src/orders/orders.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './entities/order.entity';
 
 describe('OrdersService', () => {
   let service: OrdersService;
 
+  const mockOrderRepository: Partial<Repository<Order>> = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(Order),
+          useValue: mockOrderRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
